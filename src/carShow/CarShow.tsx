322660a94b568c8523cf21b5a3cfdaedbf646853
Naoya Ono <ono.naoyaa@gmail.com>
2023-03-14 13:42:22 +0800

    Car show ground and its textures

--- src/carShow/CarShow.tsx
+++ src/carShow/CarShow.tsx
@@ -1,4 +1,5 @@
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import Ground from './Ground';
 
 const CarShow = () => {
   return (
@@ -7,10 +8,29 @@ const CarShow = () => {
 
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color="red" />
-      </mesh>
+      <color args={[0, 0, 0]} attach="background" />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
     </>
   );
 };
