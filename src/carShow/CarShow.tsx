f6372443995febe850bd2277b345a05207444b2a
Naoya Ono <ono.naoyaa@gmail.com>
2025-08-25 20:13:29 +0800

    small tweak

--- src/carShow/CarShow.tsx
+++ src/carShow/CarShow.tsx
@@ -15,7 +15,7 @@ import {
 } from '@react-three/postprocessing';
 import { BlendFunction } from 'postprocessing';
 import { Vector2 } from 'three';
-import FloatingGrid from './FloatingGrid';
+// import FloatingGrid from './FloatingGrid';
 
 const CarShow = () => {
   return (
@@ -41,11 +41,11 @@ const CarShow = () => {
 
       <Rings />
       <Boxes />
-      <FloatingGrid />
+      {/* <FloatingGrid /> */}
 
       <spotLight
         color={[1, 0.25, 0.7]}
-        intensity={1.5}
+        intensity={50}
         angle={0.6}
         penumbra={0.5}
         position={[5, 5, 0]}
@@ -55,7 +55,7 @@ const CarShow = () => {
 
       <spotLight
         color={[0.14, 0.5, 1]}
-        intensity={2}
+        intensity={100}
         angle={0.6}
         penumbra={0.5}
         position={[-5, 5, 0]}
@@ -68,7 +68,7 @@ const CarShow = () => {
       <EffectComposer>
         <Bloom
           blendFunction={BlendFunction.ADD}
-          intensity={1.3}
+          intensity={1}
           width={300}
           height={300}
           kernelSize={5}
